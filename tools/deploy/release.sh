3a8893ae35838dad443c803d03d22433a4943460
Lucas Czech <luc.czech@gmail.com>
2022-08-01 16:03:18 -0700

    Add submodule commit check to release script

--- tools/deploy/release.sh
+++ tools/deploy/release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # gappa - Genesis Applications for Phylogenetic Placement Analysis
-# Copyright (C) 2017-2019 Lucas Czech and HITS gGmbH
+# Copyright (C) 2017-2022 Lucas Czech
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -17,9 +17,9 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 # Contact:
-# Lucas Czech <lucas.czech@h-its.org>
-# Exelixis Lab, Heidelberg Institute for Theoretical Studies
-# Schloss-Wolfsbrunnenweg 35, D-69118 Heidelberg, Germany
+# Lucas Czech <lczech@carnegiescience.edu>
+# Department of Plant Biology, Carnegie Institution For Science
+# 260 Panama Street, Stanford, CA 94305, USA
 
 ####################################################################################################
 #    Helper functions
@@ -141,6 +141,33 @@ if [[ ${header_guards} != "" ]]; then
 else
     echo "Header guards okay."
 fi
+
+# Check submodule commit hashes
+function get_commit_hash() {
+    local libname=${1}
+
+    # Disect the git submodule output to get the commit that is currently checked out.
+    # This output is of the form ".<hash> libs/genesis (v0.19.0-14-g5b77dba)",
+    # where the first dot is a space, plus sign or some other git status symbol.
+    # This also sets the "return" to the outside scope... bash...
+    commit_hash=`git submodule status | grep ${libname} | sed "s/.\([0-9a-f]*\) .*/\1/g" | tr -d "\n"`
+
+    # Use two methods for checking.
+    # This fails if the submodule is checked out, but not commited.
+    # In that case, we only print a warning.
+    local hash_a=`git ls-files -s libs/${libname} | cut -d" " -f 2`
+    local hash_b=`git ls-tree master libs/${libname} | awk -F "[ \t]" '{print $3}'`
+
+    if [[ ${hash_a} != ${hash_b} ]]; then
+        echo -e "\e[31mProblem with commit hash for ${libname}: ${hash_a} != ${hash_b}\e[0m"
+    fi
+    if [[ ${hash_a} != ${commit_hash} ]]; then
+        echo -e "\e[31mNot yet commited submodule ${libname}: ${commit_hash} > ${hash_a}\e[0m"
+    fi
+}
+
+get_commit_hash "CLI11"
+get_commit_hash "genesis"
 
 ####################################################################################################
 #    Build all
